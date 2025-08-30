293129c43f9720513ca0851209d5e4f5fb9d4c10
vladyslavvvvvvv <zebrochka228333@gmail.com>
2025-08-30 18:02:04 +0200

    added delete button

--- blogsphere_app/views.py
+++ blogsphere_app/views.py
@@ -31,7 +31,6 @@ class PostListView(ListView):
     model = Post
     template_name = "blogsphere_app/mainpage.html"
     context_object_name = "all_posts"
-    paginate_by = 7
 
     def get_queryset(self):
         if self.request.GET.get("search", False):
@@ -77,12 +76,17 @@ class PostDetailView(DetailView):
 class PostEditView(UpdateView,UserIsOwnerMixin):
     model = Post
     form_class = CreateupdatePostForm
-    template_name = "blogsphere_app/createpost.html"
+    fields = ['title', 'describition', 'file']
+    template_name = "blogsphere_app/post_confirm_delete.html.html"
     success_url = reverse_lazy("main-page")
+    def get_queryset(self):
+        return Post.objects.filter(user=self.request.user)
 
 class DeletePostView(DeleteView,UserIsOwnerMixin):
     model = Post
     success_url = reverse_lazy("main-page")
+    def get_queryset(self):
+        return Post.objects.filter(user=self.request.user)
 
 class CommentCreateView(CreateView, UserIsOwnerMixin):
     model = Comment
